7118fbaf266f25c033cf3aab70b46e3840c87350
Sangram <sangram15.behera@gmail.com>
2022-05-23 11:42:22 +0530

    UseCase1 Added

---
+++ UserRegistration/UserRegistration/UserValidation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class Validation
+    {
+
+
+        public void ValidateFirstName()
+        {
+
+            Console.WriteLine("Enter First Name : ");
+            string name = Console.ReadLine();
+
+            string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{0-6}$";
+            Regex reg = new Regex(FIRSTNAME_REGEX);
+
+            if (reg.IsMatch(name))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name Found");
+            }
+
+        }
+    }
+}
