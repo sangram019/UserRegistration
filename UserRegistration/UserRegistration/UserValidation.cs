ef2f513b25e28f2a7f1099b95d8e649ce9ff57ed
Sangram <sangram15.behera@gmail.com>
2022-05-23 13:37:44 +0530

    UseCase-4

--- UserRegistration/UserRegistration/UserValidation.cs
+++ UserRegistration/UserRegistration/UserValidation.cs
@@ -11,57 +11,75 @@ namespace UserRegistrationRegex
     {
 
 
-        public void ValidateFirstName()
+        //public void ValidateFirstName()
+        //{
+
+        //    Console.WriteLine("Enter First Name : ");
+        //    string name = Console.ReadLine();
+
+        //    string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{0-6}$";
+        //    Regex reg = new Regex(FIRSTNAME_REGEX);
+
+        //    if (reg.IsMatch(name))
+        //    {
+        //        Console.WriteLine("First Name is valid");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Invalid Name Found");
+        //    }
+
+        //}
+        //public void ValidateLastName()
+        //{
+        //    Console.WriteLine("Enter Last Name : ");
+        //    string Name = Console.ReadLine();
+
+        //    string LastName = "^[A-Z]{1}[a-zA-Z]{6,}$";
+        //    Regex reg = new Regex(LastName);
+
+        //    if (reg.IsMatch(Name))
+        //    {
+        //        Console.WriteLine("Last Name is valid");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Verify Last Name Again");
+        //    }
+        //}
+        //public void ValidateEmail()
+        //{
+        //    Console.WriteLine("Enter Email ID : ");
+        //    string email = Console.ReadLine();
+
+        //    string emailID = "^[0-9a-z]+[.+-_]{0,1}[0-9a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
+        //    Regex reg = new Regex(emailID);
+
+        //    if (reg.IsMatch(email))
+        //    {
+        //        Console.WriteLine("E-mail ID is valid");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Invalid E-mail Id");
+        //    }
+
+        //}
+        public void ValidateMobileNumber()
         {
+            Console.WriteLine("Enter Mobile Number : ");
+            string MobileNumber = Console.ReadLine();
 
-            Console.WriteLine("Enter First Name : ");
-            string name = Console.ReadLine();
+            string MOBILENUMBER_REGEX = "^[+]{1}[1-9]{2}[-. ]{1}[1-9]{2}[0-9]{8}$";
+            Regex reg = new Regex(MOBILENUMBER_REGEX);
 
-            string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{0-6}$";
-            Regex reg = new Regex(FIRSTNAME_REGEX);
-
-            if (reg.IsMatch(name))
+            if (reg.IsMatch(MobileNumber))
             {
-                Console.WriteLine("First Name is valid");
+                Console.WriteLine("Mobile Number is valid");
             }
             else
             {
-                Console.WriteLine("Invalid Name Found");
-            }
-
-        }
-        public void ValidateLastName()
-        {
-            Console.WriteLine("Enter Last Name : ");
-            string Name = Console.ReadLine();
-
-            string LastName = "^[A-Z]{1}[a-zA-Z]{6,}$";
-            Regex reg = new Regex(LastName);
-
-            if (reg.IsMatch(Name))
-            {
-                Console.WriteLine("Last Name is valid");
-            }
-            else
-            {
-                Console.WriteLine("Verify Last Name Again");
-            }
-        }
-        public void ValidateEmail()
-        {
-            Console.WriteLine("Enter Email ID : ");
-            string email = Console.ReadLine();
-
-            string emailID = "^[0-9a-z]+[.+-_]{0,1}[0-9a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
-            Regex reg = new Regex(emailID);
-
-            if (reg.IsMatch(email))
-            {
-                Console.WriteLine("E-mail ID is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid E-mail Id");
+                Console.WriteLine("Verify Mobile Number Again");
             }
 
         }
