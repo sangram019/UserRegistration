82bf56c4d34fd8c7ae686fcfcac7f8f1adf27cfc
Sangram <sangram15.behera@gmail.com>
2022-05-23 11:36:37 +0530

    UseCase-1 Added

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistrationRegex;
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to User Registration Problem");
+
+        Validation validation = new Validation();
+        validation.ValidateFirstName();
+    }
+}
