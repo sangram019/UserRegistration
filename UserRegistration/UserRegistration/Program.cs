bf6344d74d56c893983ee8947a5c73047d02346d
Sangram <sangram15.behera@gmail.com>
2022-05-24 11:14:10 +0530

    UseCase-7 One Numaric in password

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿See https://aka.ms/new-console-template for more information
 using UserRegistrationRegex;
 class Program
 {
@@ -7,14 +7,14 @@ class Program
 
         Console.WriteLine("Welcome to User Registration Problem");
 
-        //Validation validation = new Validation();
-        //validation.ValidateFirstName();
+        Validation validation = new Validation();
+        validation.ValidateFirstName();
 
-        //Validation validation1 = new Validation();
-        //validation1.ValidateLastName();
+        Validation validation1 = new Validation();
+        validation1.ValidateLastName();
 
-        //Validation validation2 = new Validation();
-        //validation2.ValidateEmail();
+        Validation validation2 = new Validation();
+        validation2.ValidateEmail();
 
         Validation validation3 = new Validation();
         validation3.ValidateMobileNumber();
@@ -24,5 +24,8 @@ class Program
 
         Validation validation5 = new Validation();
         validation5.ValidatePassword1();
+
+        Validation validation6 = new Validation();
+        validation6.ValidatePassword2();
     }
 }
