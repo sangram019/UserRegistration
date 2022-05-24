38d663a633ac587cb26c5e5f43662b7ecee383eb
Sangram <sangram15.behera@gmail.com>
2022-05-24 11:15:51 +0530

    UseCase-7 One Numaric in password

--- UserRegistration/UserRegistration/UserValidation.cs
+++ UserRegistration/UserRegistration/UserValidation.cs
@@ -11,60 +11,60 @@ namespace UserRegistrationRegex
     {
 
 
-        //public void ValidateFirstName()
-        //{
+        public void ValidateFirstName()
+        {
 
-        //    Console.WriteLine("Enter First Name : ");
-        //    string name = Console.ReadLine();
+            Console.WriteLine("Enter First Name : ");
+            string name = Console.ReadLine();
 
-        //    string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{0-6}$";
-        //    Regex reg = new Regex(FIRSTNAME_REGEX);
+            string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{0-6}$";
+            Regex reg = new Regex(FIRSTNAME_REGEX);
 
-        //    if (reg.IsMatch(name))
-        //    {
-        //        Console.WriteLine("First Name is valid");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Invalid Name Found");
-        //    }
+            if (reg.IsMatch(name))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name Found");
+            }
 
-        //}
-        //public void ValidateLastName()
-        //{
-        //    Console.WriteLine("Enter Last Name : ");
-        //    string Name = Console.ReadLine();
+        }
+        public void ValidateLastName()
+        {
+            Console.WriteLine("Enter Last Name : ");
+            string Name = Console.ReadLine();
 
-        //    string LastName = "^[A-Z]{1}[a-zA-Z]{6,}$";
-        //    Regex reg = new Regex(LastName);
+            string LastName = "^[A-Z]{1}[a-zA-Z]{6,}$";
+            Regex reg = new Regex(LastName);
 
-        //    if (reg.IsMatch(Name))
-        //    {
-        //        Console.WriteLine("Last Name is valid");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Verify Last Name Again");
-        //    }
-        //}
-        //public void ValidateEmail()
-        //{
-        //    Console.WriteLine("Enter Email ID : ");
-        //    string email = Console.ReadLine();
+            if (reg.IsMatch(Name))
+            {
+                Console.WriteLine("Last Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Verify Last Name Again");
+            }
+        }
+        public void ValidateEmail()
+        {
+            Console.WriteLine("Enter Email ID : ");
+            string email = Console.ReadLine();
 
-        //    string emailID = "^[0-9a-z]+[.+-_]{0,1}[0-9a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
-        //    Regex reg = new Regex(emailID);
+            string emailID = "^[0-9a-z]+[.+-_]{0,1}[0-9a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
+            Regex reg = new Regex(emailID);
 
-        //    if (reg.IsMatch(email))
-        //    {
-        //        Console.WriteLine("E-mail ID is valid");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Invalid E-mail Id");
-        //    }
+            if (reg.IsMatch(email))
+            {
+                Console.WriteLine("E-mail ID is valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid E-mail Id");
+            }
 
-        //}
+        }
         public void ValidateMobileNumber()
         {
             Console.WriteLine("Enter Mobile Number : ");
@@ -117,6 +117,22 @@ namespace UserRegistrationRegex
                 Console.WriteLine("Verify Password Again");
             }
         }
+        public void ValidatePassword2()
+        {
+            Console.WriteLine("Enter Password : ");
+            string password = Console.ReadLine();
+
+            string PASSWORD = "^[a-zA-z0-9]{8,}$";
+            Regex reg = new Regex(PASSWORD);
+
+            if (reg.IsMatch(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Verify Password Again");
+            }
+        }
     }
 }
-    
